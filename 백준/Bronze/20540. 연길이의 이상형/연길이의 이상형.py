038e9423e7a150466e857770bb5e2ecdca705ac4
jinny <165622495+JINI0617@users.noreply.github.com>
2025-07-06 19:45:33 +0900

    [Bronze III] Title: 연길이의 이상형, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/20540. 연길이의 이상형/연길이의 이상형.py
@@ -0,0 +1,21 @@
+s = input()
+
+if s[0] == 'E':
+    print('I', end = '')
+else:
+    print('E', end = '')
+
+if s[1] == 'N':
+    print('S', end = '')
+else:
+    print('N', end = '')
+
+if s[2] == 'F':
+    print('T', end = '')
+else:
+    print('F', end = '')
+
+if s[3] == 'P':
+    print('J', end = '')
+else:
+    print('P', end = '')
